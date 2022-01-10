c02be09b3eb7728a1f058ab19a87cf8206e62469
maroomir <maroomir@gmail.com>
2022-01-10 20:04:12 +0900

    Init commit

---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RobotController : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Joint
+    {
+        public string inputAxis;
+        public GameObject robotPart;
+    }
+    public Joint[] joints;
+
+
+    // CONTROL
+
+    public void StopAllJointRotations()
+    {
+        for (int i = 0; i < joints.Length; i++)
+        {
+            GameObject robotPart = joints[i].robotPart;
+            UpdateRotationState(RotationDirection.None, robotPart);
+        }
+    }
+
+    public void RotateJoint(int jointIndex, RotationDirection direction)
+    {
+        StopAllJointRotations();
+        Joint joint = joints[jointIndex];
+        UpdateRotationState(direction, joint.robotPart);
+    }
+
+    public void RotateJoint(int nIndex, RotationDirection eDirection, float fSpeed)
+    {
+        StopAllJointRotations();
+        Joint pJoint = joints[nIndex];
+        UpdateRotationState(eDirection, pJoint.robotPart, fSpeed);
+    }
+
+    // HELPERS
+
+    static void UpdateRotationState(RotationDirection direction, GameObject robotPart)
+    {
+        ArticulationJointController jointController = robotPart.GetComponent<ArticulationJointController>();
+        jointController.rotationState = direction;
+    }
+
+    static void UpdateRotationState(RotationDirection eDirection, GameObject pRobotPart, float fSpeed)
+    {
+        ArticulationJointController pJoint = pRobotPart.GetComponent<ArticulationJointController>();
+        pJoint.speed = fSpeed;
+        pJoint.rotationState = eDirection;
+    }
+}
