a4f3ca33238f55ff3b53c8f509682b0498bab317
maroomir <maroomir@gmail.com>
2022-01-29 16:43:51 +0900

    Modify the arguments supported of DHParameter

--- Assets/Scripts/Commons/MathFactory.cs
+++ Assets/Scripts/Commons/MathFactory.cs
@@ -4,13 +4,13 @@ public class KinematicsCalculator
 {
     public class DHParameter
     {
-        // Distance of the Z-axis based on the X-axis
+        // Distance of the Y(Z)-axis based on the Z(X)-axis
         public float a = 0;
-        // Rotation angle on the Z-axis based on the X-axis
+        // Rotation angle on the Y(Z)-axis based on the Z(X)-axis
         public float alpha = 0;
-        // Distance of the X-axis based on the Z-axis
+        // Distance of the Z(X)-axis based on the Y(Z)-axis
         public float d = 0;
-        // Rotation angle on the X-axis based on the Z-axis
+        // Rotation angle on the Z(X)-axis based on the Y(Z)-axis
         public float theta = 0;
 
         private float SpanAngle(float fAngle)
@@ -29,11 +29,13 @@ public class KinematicsCalculator
         public DHParameter(GameObject pPrevAxis, GameObject pCurrAxis)
         {
             Vector3 pDistance = pCurrAxis.transform.position - pPrevAxis.transform.position;
-            Vector3 pRotation = pCurrAxis.transform.eulerAngles - pPrevAxis.transform.eulerAngles;
-            a = pDistance.z;
-            alpha = SpanAngle(pRotation.z);
-            d = pDistance.x;
-            theta = SpanAngle(pRotation.x);
+            ArticulationBody pPrevBody = pPrevAxis.GetComponent<ArticulationBody>();
+            ArticulationBody pCurrBody = pCurrAxis.GetComponent<ArticulationBody>();
+            Vector3 pRotation = pCurrBody.anchorRotation.eulerAngles - pPrevBody.anchorRotation.eulerAngles;
+            a = pDistance.y;
+            alpha = SpanAngle(pRotation.y);
+            d = pDistance.z;
+            theta = SpanAngle(pRotation.z);
         }
 
         public Vector3 RotationAxis => new Vector3(theta / 90.0F, 0.0F, alpha / 90.0F);
@@ -44,12 +46,11 @@ public class KinematicsCalculator
     public Vector3 BasePosition { get; set; }
     public int Length => Parameters.Length;
 
-    private readonly float _fSampleDistance = 0.0F;
-    private readonly float _fThreshold = 0.0F;
-    private readonly float _fLearningRate = 0.0F;
+    private readonly float _fSampleDistance;
+    private readonly float _fThreshold;
 
-    public KinematicsCalculator(GameObject[] pJoints, float fSampleDistance = 0.1F, float fThreshold = 0.1F, float fLearningRate = 1.0F)
-        : this(fSampleDistance, fThreshold, fLearningRate)
+    public KinematicsCalculator(GameObject[] pJoints, float fSampleDistance = 0.1F, float fThreshold = 0.1F)
+        : this(fSampleDistance, fThreshold)
     {
         int nLength = pJoints.Length - 1;
         BasePosition = pJoints[0].transform.position;
@@ -60,17 +61,16 @@ public class KinematicsCalculator
         }
     }
 
-    public KinematicsCalculator(DHParameter[] pParams, float fSampleDistance = 0.1F, float fThreshold = 0.1F, float fLearningRate = 1.0F)
-        : this(fSampleDistance, fThreshold, fLearningRate)
+    public KinematicsCalculator(DHParameter[] pParams, float fSampleDistance = 0.1F, float fThreshold = 0.1F)
+        : this(fSampleDistance, fThreshold)
     {
         Parameters = pParams;
     }
 
-    public KinematicsCalculator(float fSampleDistance = 0.1F, float fThreshold = 0.1F, float fLearningRate = 1.0F)
+    public KinematicsCalculator(float fSampleDistance = 0.1F, float fThreshold = 0.1F)
     {
         _fSampleDistance = fSampleDistance;
         _fThreshold = fThreshold;
-        _fLearningRate = fLearningRate;
     }
 
     public Vector3 ForwardKinematics(float[] pAngles)
@@ -98,28 +98,27 @@ public class KinematicsCalculator
         if (Length != pPrevAngles.Length)
             throw new MissingReferenceException("Invalid counter of DH Parameters");
         float[] pResults = pPrevAngles;
-        while (GetDistance(pInput, pResults) > _fThreshold)
+        while (true)
         {
             for (int i = 0; i < Length; i++)
-            {
-                float fGradient = GetGradient(pInput, i, pResults);
-                pResults[i] -= _fLearningRate * fGradient;
-            }
+                pResults[i] += (float)PartialGradient(pInput, i, pResults);
+            if(PartialDistance(pInput, pResults) <= _fThreshold)
+                break;
         }
         return pResults;
     }
 
-    private float GetDistance(Vector3 pTargetPos, float[] pAngles)
+    private double PartialDistance(Vector3 pTargetPos, float[] pAngles)
     {
         Vector3 pConvertPos = ForwardKinematics(pAngles);
         return Vector3.Distance(pTargetPos, pConvertPos);
     }
 
-    private float GetGradient(Vector3 pTargetPos, int iIndex, float[] pAngles)
+    private double PartialGradient(Vector3 pTargetPos, int iIndex, float[] pAngles)
     {
-        float fPrevDist = GetDistance(pTargetPos, pAngles);
+        double fPrevDist = PartialDistance(pTargetPos, pAngles);
         pAngles[iIndex] += _fSampleDistance;
-        float fNextDist = GetDistance(pTargetPos, pAngles);
+        double fNextDist = PartialDistance(pTargetPos, pAngles);
         return (fNextDist - fPrevDist) / _fSampleDistance;
     }
 }
